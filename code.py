65df847f762e12b0e68c12fd0ce2e5cc766f3e4c
laharinomula <135495849+laharinomula@users.noreply.github.com>
2023-06-04 13:06:57 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,13 @@
-
+def anagram_grouping(words):
+    d = {}
+    for word in words:
+        sorted_word = ''.join(sorted(word))
+        if sorted_word in d:
+            d[sorted_word].append(word)
+        else:
+            d[sorted_word] = [word]
+    return list(d.values())
+words = ["cat","dog","tac","god","act","Cat"]
+words=[i.lower() for i in words]
+words=list(set(words))
+print(anagram_grouping(words))
