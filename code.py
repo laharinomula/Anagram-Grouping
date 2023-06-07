9ed061c04e8c1115fb142aa7e75196b78960c6ab
laharinomula <135495849+laharinomula@users.noreply.github.com>
2023-06-07 23:26:45 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,13 +1,21 @@
 def anagram_grouping(words):
     d = {}
     for word in words:
-        sorted_word = ''.join(sorted(word))
+        sorted_word = ''.join(sorted(word))	
         if sorted_word in d:
             d[sorted_word].append(word)
         else:
             d[sorted_word] = [word]
-    return list(d.values())
-words = ["cat","dog","tac","god","act","Cat"]
+    return list(d.values()) 
+N=int(input("enter list size:"))
+words=[]
+for i in range(0,N):
+	print("enter word:")
+	word=input("")
+	if len(word)>=1 and len(word)<=100:
+		words.append(word)
+	else:
+		print("enter word length between 1 and 100")
 words=[i.lower() for i in words]
 words=list(set(words))
 print(anagram_grouping(words))
